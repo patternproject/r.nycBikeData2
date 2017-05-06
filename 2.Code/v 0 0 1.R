ab0bf1529bccc8afdb157977a89ccc7f75acbd95
patternproject <patternproject@yahoo.com>
2017-05-06 17:35:29 +0500

    1. Adding CSV file to gitignore. 2. diff.txt and search.txt are used to get names of large files 3. Not including .csv (the large file) in this committ

--- 2.Code/v 0 0 1.R
+++ 2.Code/v 0 0 1.R
@@ -1,8 +1,9 @@
 ## `````````````````````````````````````````````
 #### Read Me ####
 ## `````````````````````````````````````````````
-## This is the template file. Rename it as v 0 0 1.R in Code dir
-## And start codingaaa more changes and more and more
+## Extending NYC Bike Data Analysis
+## 
+## http://toddwschneider.com/posts/a-tale-of-twenty-two-million-citi-bikes-analyzing-the-nyc-bike-share-system/
 ## `````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
@@ -11,8 +12,8 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
 pacman::p_load(readxl)
-
-
+pacman::p_load(stringr)
+pacman::p_load(RCurl)
 ## `````````````````````````````````````````````
 
 ## `````````````````````````````````````````````
@@ -28,13 +29,13 @@ pacman::p_load(readxl)
 ### Setting Working Dir
 # SRC: http://stackoverflow.com/questions/17605563/efficiently-convert-backslash-to-forward-slash-in-r
 # 1. At the prompt copy and paste windows path of project root folder
-x <- readline()
+# x <- readline()
 # 2. 
-my.dir <- gsub("\\\\", "/", x)
+# my.dir <- gsub("\\\\", "/", x)
 # 3. 
-setwd(my.dir)
+# setwd(my.dir)
 # 4. check
-getwd()
+# getwd()
 ## ````````````````````
 
 
@@ -51,13 +52,24 @@ getwd()
 # For R Projects the working directory is always set to the root folder, 
 # so in order to load our data into R we need to first go into the “data” folder 
 # and then read in the data file, thus our call is “data/my.data.file.txt”
+URL = "https://s3.amazonaws.com/tripdata/201601-citibike-tripdata.zip"
 
+myzip <- unz(URL, filename=basename(URL))
+download.file(URL, destfile="1. Data/test.zip")
+f.name = unzip("1. Data/test.zip", exdir ="1. Data")
+# readr from tidyverse supports read_csv()
+df.1 = read_csv(f.name,col_names = TRUE)
+
+# remove zip file
+fn <- "1. Data/test.zip"
+if (file.exists(fn)) file.remove(fn)
 ## `````````````````````````````````````````````
 
 
 ## `````````````````````````````````````````````
 #### Manipulate Data ####
 ## `````````````````````````````````````````````
+str(df.1)
 ## `````````````````````````````````````````````
 
 
